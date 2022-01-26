a664a6f281c0c742297b3e962da4f650e8ed3860
ChiaYaoXing <57871318+ChiaYaoXing@users.noreply.github.com>
2022-01-26 21:14:13 +0100

    Added get_movie() signature to interface, code formatted

--- src/interfaces/IMovieDatabase.java
+++ src/interfaces/IMovieDatabase.java
@@ -1,14 +1,17 @@
 package interfaces;
 
 import java.util.Date;
+import java.util.List;
 
 public interface IMovieDatabase {
 	
 	public boolean rate(int rating, String uid, String mid, String comment);
 	public boolean check_rate(int rating, String uid, String mid, String comment);
 	
-	 public boolean addMovie(String title, String director, String actors, Date publishingDate, int mid);
-	 public boolean check_movie(String title, String director, String actors, Date publishingDate, int mid);
+	public boolean addMovie(String title, String director, String actors, Date publishingDate, int mid);
+	public boolean check_movie(String title, String director, String actors, Date publishingDate, int mid);
+	
+	public List<Movie> get_movie();
 	
 	
 }
