f0d5df620b03201393d0b312febba7ee066092a6
ChiaYaoXing <57871318+ChiaYaoXing@users.noreply.github.com>
2022-01-26 21:15:49 +0100

    Fixed Import statement

--- src/dbadapter/MD_Adapter.java
+++ src/dbadapter/MD_Adapter.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
 
 import interfaces.IMovieDatabase;
 
